2ac72dd45107acd99ad086561dd2ce6e907ecb54
kunzm13 <kunzm13@bfh.ch>
2015-06-06 16:35:45 +0200

    created scene2 with checkpoints

---
+++ Assets/Scripts/Checkpoints.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoints : MonoBehaviour {
+
+	private GuiBehaviour _guiScript;
+	private string tempCheckpoint;
+	
+	void Start () {	
+		_guiScript = GameObject.Find ("ScriptContainer").GetComponent<GuiBehaviour> ();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "PlayerCol") {
+			tempCheckpoint = _guiScript.recentCheckpoint;
+
+			if ((tempCheckpoint == "" || tempCheckpoint == "Finished") && this.name == "Checkpoint1") {
+				_guiScript.recentCheckpoint = this.name;
+				_guiScript.updateCheckpoint1();
+				Debug.Log("Checkpoint: " + this.name);
+			}
+
+			if (tempCheckpoint == "Checkpoint1" && this.name == "Checkpoint2") {
+				_guiScript.recentCheckpoint = this.name;
+				_guiScript.updateCheckpoint2();
+				Debug.Log("Checkpoint: " + this.name);
+			}
+			
+			if (tempCheckpoint == "Checkpoint2" && this.name == "Checkpoint3") {
+				_guiScript.recentCheckpoint = this.name;
+				_guiScript.updateCheckpoint3();
+				Debug.Log("Checkpoint: " + this.name);
+			}
+			
+			if (tempCheckpoint == "Checkpoint3" && this.name == "Finished") {
+				_guiScript.recentCheckpoint = this.name;
+				_guiScript.resetCheckpoints();
+				Debug.Log("Checkpoint: " + this.name);
+			}
+		}
+	}
+}
--- Assets/Scripts/GuiBehaviour.cs
+++ Assets/Scripts/GuiBehaviour.cs
@@ -3,44 +3,75 @@ using System.Collections;
 
 public class GuiBehaviour : MonoBehaviour {
 
-	public float pastTime = 0;
+	public float pastTime = 300;
 	public GUIText guiTime;
 	public WheelCollider flWheelCol;
 
-	private CarBehaviour _carScript;
+	//Checkpoints
+	public GUIText checkPoint1Text;
+	public GUIText checkPoint2Text;
+	public GUIText checkPoint3Text;
+	public GUIText checkPoint1Value;
+	public GUIText checkPoint2Value;
+	public GUIText checkPoint3Value;
+	public GUIText lapsText;
 
+	public int laps = 0;
+	public string recentCheckpoint;
+
+	private CarBehaviour _carScript;
+	private float _timeToBeat = 300;
 	private bool _isFinished = false;
+	private string _textTimeToBeat;
+	private int _laps = 0;
 
 	// Use this for initialization
-	void Start () {
-
+	void Start() {
 		_carScript = GameObject.Find ("COSWORTH").GetComponent<CarBehaviour> ();
-	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		if (_carScript.enabled) {
-			WheelHit hit;
-			if (flWheelCol.GetGroundHit (out hit)) {
-		
-				if (hit.collider.gameObject.tag == "Finish") {
-					_isFinished = true;
-				}
-				if (!_isFinished)
-					pastTime += Time.deltaTime;
-
-				guiTime.text = pastTime.ToString ("0.0");
-
-			}
-
+			_timeToBeat -= Time.deltaTime;
+			_textTimeToBeat = _timeToBeat.ToString ("0.0");
+			guiTime.text = "Time to beat: " + _textTimeToBeat;
 		}
 	}
 
 	public void OnButtonMenuClick() {
-	
 		Application.LoadLevel (0);
-	
 	}
 
+	public void updateCheckpoint1() {
+		checkPoint1Text.enabled = true;
+		checkPoint1Value.text = _textTimeToBeat;
+		checkPoint1Value.enabled = true;
+	}
+
+	public void updateCheckpoint2() {
+		checkPoint2Text.enabled = true;
+		checkPoint2Value.text = _textTimeToBeat;
+		checkPoint2Value.enabled = true;
+	}
+
+	public void updateCheckpoint3() {
+		checkPoint3Text.enabled = true;
+		checkPoint3Value.text = _textTimeToBeat;
+		checkPoint3Value.enabled = true;
+	}
+
+	public void resetCheckpoints() {
+		checkPoint1Text.enabled = false;
+		checkPoint2Text.enabled = false;
+		checkPoint3Text.enabled = false;
+
+		checkPoint1Value.enabled = false;
+		checkPoint2Value.enabled = false;
+		checkPoint3Value.enabled = false;
+
+		_laps++;
+
+		lapsText.text = "Laps: " + _laps;
+	}
 }
